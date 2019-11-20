d32a50adca893455b74b738690513916c63275fb
N00170170 <N00170170@users.noreply.github.com>
2019-11-20 17:10:52 +0000

    Finish first video

--- app/User.php
+++ app/User.php
@@ -36,4 +36,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles(){
+      return $this->belongsToMany('App\Role', 'user_role');
+    }
+
+    public function hasRole($role){
+      return null !== $this->roles()->where('name', $role)->first();
+    }
+
+    public function hasAnyRole($roles){
+      return null !== $this->roles()->whereIn('name', $roles)->first();
+    }
+
+    public function authoriseRoles($roles){
+      if(is_array($roles)){
+        return $this->hasAnyRoles($roles) || abort(401, 'this action is unauthorised');
+      }
+        return $this->hasRole($roles) || abort(401, 'this action is unauthorised');
+    }
+
 }
