28ac84f80ef27ef2d62a6211a113327a32e66d7e
N00170170 <N00170170@users.noreply.github.com>
2019-12-04 18:39:36 +0000

    Add authorisation for all Admin Controllers

--- app/Http/Controllers/Admin/DoctorController.php
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -10,6 +10,11 @@ use App\Role;
 
 class DoctorController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role:admin');
+    }
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/Admin/VisitController.php
+++ app/Http/Controllers/Admin/VisitController.php
@@ -10,6 +10,12 @@ use App\Doctor;
 
 class VisitController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role:admin');
+    }
     /**
      * Display a listing of the resource.
      *
